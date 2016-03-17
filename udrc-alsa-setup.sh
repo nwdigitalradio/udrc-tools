ae06ea0e53e40dbd1d97367c4d31ad782e77672c
Jeremy McDermond <nh6z@nh6z.net>
2016-03-17 11:00:19 -0700

    Change the active inputs
    Apparently we need DISCOUNT and TONEIN to be on, not AFOUT and AFIN.

--- udrc-alsa-setup.sh
+++ udrc-alsa-setup.sh
@@ -6,12 +6,12 @@ sset 'ADC Level' 24
 sset 'LO Driver Gain' 6
 
 #  Turn on AFOUT
-sset 'Left Input Mixer IN1_L P' on
-sset 'Left Input Mixer CM1_L N' on
+sset 'Left Input Mixer IN1_L P' off
+sset 'Left Input Mixer CM1_L N' off
 
 #  Turn off DISCOUT
-sset 'Right Input Mixer IN1_R P' off
-sset 'Right Input Mixer CM1_R N' off 
+sset 'Right Input Mixer IN1_R P' on
+sset 'Right Input Mixer CM1_R N' on 
 
 #  Turn off unnecessary pins
 sset 'Left Input Mixer IN2_L P' off
@@ -24,8 +24,8 @@ sset 'Right Input Mixer IN3_L N' off
 sset 'Right Input Mixer IN3_R P' off
 
 #  Turn on AFIN
-sset 'LOL Output Mixer L_DAC' on
+sset 'LOL Output Mixer L_DAC' off
 
 #  Turn off TONEIN
-sset 'LOR Output Mixer R_DAC' off
+sset 'LOR Output Mixer R_DAC' on
 EOF
