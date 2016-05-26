fdfd5d16389509fd98aabb5ee6c6ec4118c497a4
John Hays (K7VE) <john@hays.org>
2016-05-25 18:04:19 -0700

    Update udrc-alsa-setup.sh

--- scripts/udrc-alsa-setup.sh
+++ scripts/udrc-alsa-setup.sh
@@ -4,7 +4,7 @@ amixer -c udrc -s << EOF
 #  If you change your level settings, make a new script, save, and run it
 #  Set input and output levels to 0dB
 sset 'ADC Level' 5.5dB
-sset 'LO Driver Gain' -1dB
+sset 'LO Driver Gain' 0.0dB
 sset 'PCM' 0.0dB
 
 #  Turn off AFOUT
