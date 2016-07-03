8f2286acf6bcee1a85023edd9e6f90bead84b027
John Hays <john@hays.org>
2016-07-03 14:37:45 -0700

    Optimized

--- scripts/upnp.sh
+++ scripts/upnp.sh
@@ -10,7 +10,6 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 fi
 	
 # Argument '$1' is the interface to the Internet, e.g. eth0 or wlan0
-
 IPADDR=`ifconfig $1 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | sed 's/inet addr://'`
 
 #Remote Control is specific to ircDDBGateway and should be set to match the
@@ -23,26 +22,21 @@ DCSPorts=`seq 30051 30059`
 DPlusPorts=`seq 20001 20009`
 CCSPorts=`seq 30061 30065`
 CSRouting="40000"
-
-#Remove any collection of ports which are not used in your gateway
-UDPPorts="$DExtraPorts $DCSPorts $DPlusPorts $CCSPorts $CSRouting $RemoteControl"
-
-#If you are running D-STAR DATA
 IPRouting="40001"
 
-#echo $IPADDR
+#Remove any collection of ports which are not used in your gateway
+UDPPorts="$DPlusPorts $DExtraPorts $DCSPorts $CCSPorts $CSRouting $RemoteControl"
+
+for j in $UDPPorts; do
+        PORTS="$PORTS $j UDP"
+done
+
+#If you are running D-STAR DATA Uncomment
+#PORTS="$PORTS $IPRouting TCP"
 
 if [ "$2" = "start" ]; then
-	for p in $UDPPorts; do
-		upnpc -a $IPADDR $p $p UDP
-	done
-# Uncomment the following line if you need D-STAR DATA ROUTING
-#	upnpc -a $IPADDR $IPRouting $IPRouting TCP
+	upnpc -r $PORTS
 else
-	for p in $UDPPorts; do
-                upnpc -d $p UDP
-        done
-# Uncomment the following line if you have enabled D-STAR DATA ROUTING
-#       upnpc -d $IPRouting TCP
+        upnpc -d $PORTS
 fi
 echo done
